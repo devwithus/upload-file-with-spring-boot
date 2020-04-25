915058c0e5c33a71d16e46c2f9c6b5df2e24bf21
azhwani <azhwani@devwithus.com>
2020-04-25 21:36:18 +0100

    first commit

---
+++ src/main/java/com/api/downupload/DownuploadApplication.java
@@ -0,0 +1,13 @@
+package com.api.downupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownuploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DownuploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/downupload/DownuploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.downupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownuploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
